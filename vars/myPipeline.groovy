c3e3628001a277e6201c21051f03820100542d98
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-13 10:28:08 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,3 +1,45 @@
 def call() {
-    println "My Shared Library Pipeline"
+    node {
+        def mvnHome = tool 'Maven-3.9'
+
+        stage('Checkout') {
+            checkout scmGit(branches: [[name: '*/master']],
+                            extensions: [],
+                            userRemoteConfigs: [[
+                                url: 'https://github.com/ramesh9849720900/Setup-CI-CD-with-Github-Jenkins-Maven-and-Tomcat-on-AWS.git'
+                            ]])
+        }
+
+        stage('Build') {
+            sh "${mvnHome}/bin/mvn clean install -f webapp/pom.xml"
+        }
+
+        stage('Build & Analysis in SonarQube') {
+            withSonarQubeEnv('sonarserver') {
+                sh "${mvnHome}/bin/mvn clean install sonar:sonar -U -f webapp/pom.xml"
+            }
+        }
+
+        stage('Push Artifact to GitHub') {
+            dir('webapp') {
+                sh '''
+                    git config user.email "rameshrams9849@gmail.com"
+                    git config user.name "Ramesh"
+                    git fetch origin artifact-upload 
+                    git checkout -B artifact-upload origin/artifact-upload
+                    cp target/webapp.war .
+                    git add webapp.war
+                    git commit -m "Add build artifact" || echo "No changes to commit"
+                    git push https://ramesh9849720900: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@github.com/ramesh9849720900/Setup-CI-CD-with-Github-Jenkins-Maven-and-Tomcat-on-AWS.git HEAD:artifact-upload
+                '''
+            }
+        }
+
+        stage('Deploy to Tomcat') {
+            sh '''
+                sudo cp webapp/target/webapp.war /opt/tomcat/webapps/
+                sudo systemctl restart tomcat
+            '''
+        }
+    }
 }
