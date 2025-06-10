2c7ac88904c93c53767b49707ea48df40511617c
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-06-10 13:03:50 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -53,7 +53,7 @@ def call(Map config = [:]) {
             )]) {
                 withEnv([
                     'AWS_DEFAULT_REGION=ap-south-1',  // Replace if needed
-                    'CLUSTER_NAME=default'  // Replace with actual EKS cluster name
+                    'CLUSTER_NAME=ferocious-hiphop-hideout'  // Replace with actual EKS cluster name
                 ]) {
                     echo "Setting up kubeconfig for EKS"
                     sh '''
